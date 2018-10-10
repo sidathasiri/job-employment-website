b6da08897bdba698d796c9b91e9d078ed50878ad
Sidath Asiri <sidathasiri@gmail.com>
2018-10-10 22:41:03 +0530

    content added to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,30 @@ About
 @endsection
 
 @section('content')
-<h1>About</h1>
+<div class="page-header">
+  <h1>About</h1>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <p>We've made it our mission to help you find better candidates. And nobody brings you more cutting edge tools to help you do just that than Jobus. Whatever your needs are, we have the products,tools and technologies to build a bespoke solution for you, to help you find better.</p>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <p>US jobs can be hard to get from outside of the country. Getting a US work Visa is hard to do without a job in America. It's hard to get jobs in the United States without a US work Visa.Not to fear: us.jobs is here! Knowing how to start looking for US jobs is half the battle. </p>
+    </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <p>Look into what US jobs you want</p>
+    </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <p>Before anything else, you should learn about jobs in the United States you might be able to get. Search for the US jobs or job skills you're interested in on us.jobs to start. Get to know what the jobs are like, and what their pay might be like</p>
+    </div>
+</div>
 @endsection
