e3a8b81d12c52b495e8f127ad58a322b42399bb3
Sidath Asiri <sidathasiri@gmail.com>
2018-10-10 22:23:41 +0530

    finished authentication

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,17 +15,19 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
-        <li><a href="#"><i class="fa fa-phone"></i> Contact</a></li>
         <li><a href="#">About</a></li>
+        <li><a href="#"><i class="fa fa-phone"></i> Contact</a></li>
+        @if(Auth::check())
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Signin</a></li>
-            <li><a href="#">Signup</a></li>
+            <li><a href="{{route('profile')}}">Profile</a></li>
+            <li><a href="#">Add Job</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="{{route('logout')}}">Logout</a></li>
           </ul>
         </li>
+        @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
