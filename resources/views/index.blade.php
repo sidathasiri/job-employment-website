a2f2948995f3ac5f31fc0b8a2ef9733171d39ee6
Sidath Asiri <sidathasiri@gmail.com>
2018-10-10 18:24:48 +0530

    added the navagation bar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+    Job App
+@endsection
+
+@section('content')
+    <h1>It works</h1>
+    <button class="btn btn-primary">Click me</button>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    @yield('styles')
+</head>
+<body>
+@include('partials.header')
+<div class="container">
+@yield('content')
+</div>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+@yield('scripts')
+</body>
+</html>
