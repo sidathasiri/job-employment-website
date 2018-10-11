7a7de0d10ea8e35390f796acde36a325d20bbf9d
Sidath Asiri <sidathasiri@gmail.com>
2018-10-11 11:22:25 +0530

    added the slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,61 @@
   <p>From  mobile,to the web, to social, we help companies find people with customised solutions and we use the world's most advanced technology to match and search the perfect people to the right job.</p>
 </div>
 
+<h2 style="color:white">Top Jobs</h2>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    @foreach($posts as $post)
+    <li data-target="#myCarousel" data-slide-to="{{$post->id}}" class="active"></li>
+    @endforeach
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+  @foreach($posts as $post)
+    @if($post->id == 1)
+    <div class="item active">
+      <div class="row">
+        <div class="col-sm-6 col-md-12" >
+          <div class="thumbnail" style="background-color: #abf2ec" >
+            <div class="caption" >
+              <h3>{{$post->title}}</h3>
+              <p>{{$post->description}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    @else
+    <div class="item">
+      <div class="row">
+      <div class="col-sm-6 col-md-12" >
+        <div class="thumbnail" style="background-color: #abf2ec">
+          <div class="caption">
+            <h3>{{$post->title}}</h3>
+              <p>{{$post->description}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    @endif
+    @endforeach
+
+  <!-- Left and right controls -->
+  <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a> -->
+</div>
+
+<h2 style="color: white">Browse More</h2>
+
 @foreach($posts as $post)
 <!-- <div class="well"> -->
   <div class="panel panel-primary">
